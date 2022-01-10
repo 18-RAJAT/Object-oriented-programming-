0ddb21044218b1e599d2e5b085c34ffaa2c82f7f
ICONIC_RAJAT <78920780+18-RAJAT@users.noreply.github.com>
2022-01-10 12:40:59 +0530

    Add files via upload

---
+++ inline functon.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+inline void prientme(string str)
+{
+    cout<< str <<endl;
+};
+
+int main()
+{
+    for(int i=0;i<100000;++i)
+    {
+        printme<<"rajat "<<endl;
+    }
+}
